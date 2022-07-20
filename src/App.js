bfd919a1680ab420fe8ee97ee285514dd843b234
chjcago2001 <88874679+chjcago2001@users.noreply.github.com>
2022-07-20 15:45:39 +0700

    fist commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { Home , Movies , Signin , Signup , WatchView, Search , History , ErrorPage, MoviesStore , UsersStore} from './components'
+import { BrowserRouter, Routes, Route  } from 'react-router-dom'
+import { useStore, actions } from './store'
+import { useEffect, useState } from 'react'
+import { category, movieType, tvType } from './api/tmdbApi'
+import queryString from 'query-string'
+import axiosClient from './api/axiosClient'
+import axios from 'axios'
 
 function App() {
-  return (
-    <div className="App">
-     
-    </div>
-  );
+    const [state, dispatch] = useStore()
+    const location = 'location'
+    const search = 'this is my value'
+    const [ apiMovies , setApiMovies] = useState([])
+    const { movies, users, selectedId  ,  isLoading } = state
+    const formSearched =  JSON.parse(localStorage.getItem('searched')).s
+    const mid  = localStorage.getItem("mId") || ''
+
+    useEffect(  () => {
+        axios.get("https://api.themoviedb.org/3/movie/438148/videos?api_key=7972e9a71a1dc065165d7db9acbf0e99&language=en-US")
+        .then(response => console.log(response.data))
+    },[])
+   
+    return ( 
+    <BrowserRouter>
+    <Routes>
+         <Route path='/' element = {<Home/> }/> 
+         <Route path='/movies' element={<Movies/> }/> 
+         <Route path='/history' element={<History/> }/> 
+         <Route path='/signin' element={<Signin/> }/> 
+         <Route path='/signup' element={<Signup/> }/> 
+         <Route path='/admin/users' element={<UsersStore/> }/> 
+         <Route path='/admin/movies' element={<MoviesStore/> }/> 
+         <Route path={'/movie/' + mid}  element={<WatchView /> }/> 
+         <Route path={'/search/:slug'} element={<Search/>} />
+         <Route path='/*' element={<ErrorPage/> }/> 
+     </Routes>
+  </BrowserRouter>
+    )
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import 'antd/dist/antd.less';
+import 'antd/dist/antd.css';
+import "swiper/css/navigation";
+import 'swiper/css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { StoreProvider } from './store';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <StoreProvider>
+        <App />
+      </StoreProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
